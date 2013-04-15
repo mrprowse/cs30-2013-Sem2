4d13f8f746fca6ae761c60eab2d3163c87d77204
jackie <jackie@jackie-desktop>
2013-04-15 11:27:04 -0600

    Started creating variables and the fly method

--- my-gdx-game/src/com/me/mygdxgame/Bird.java
+++ my-gdx-game/src/com/me/mygdxgame/Bird.java
@@ -1,5 +1,63 @@
 package com.me.mygdxgame;
 
+import com.sun.java_cup.internal.runtime.Scanner;
+
 public class Bird {
+	
+boolean alive = true;
+// Create the bird size
+private int xStart;
+private int xEnd;
+private int yStart;
+private int yEnd;
+
+
+public void fly( ){
+	if(collide = true){//Collide is from Sayuhya's collision class
+		this.setAlive(false);//Base case 
+	}else{
+		//code to move the bird
+	}
+}
+public Boolean getAlive() {
+return alive;
+}
+
+public void setAlive(Boolean alive) {
+this.alive = alive;
+} 
+public int getxEnd() {
+return xEnd;
+}
+
+public void setxEnd(int xEnd) {
+this.xEnd = xEnd;
+}
+
+public int getxStart() {
+return xStart;
+}
+
+public void setxStart(int xStart) {
+this.xStart = xStart;
+}
+
+public int getyEnd() {
+return yEnd;
+}
+
+public void setyEnd(int yEnd) {
+this.yEnd = yEnd;
+}
+
+public int getyStart() {
+return yStart;
+}
+
+public void setyStart(int yStart) {
+this.yStart = yStart;
+}
+
+
 
 }
