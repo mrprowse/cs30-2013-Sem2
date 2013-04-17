2ae2b9edda293705be9b0e3d9b2dd1b3f52cb987
jackie <nim.jackie@gmail.com>
2013-04-17 09:46:22 -0600

    Added Listeners for arrow keys

--- my-gdx-game/src/com/me/mygdxgame/Bird.java
+++ my-gdx-game/src/com/me/mygdxgame/Bird.java
@@ -1,5 +1,6 @@
 package com.me.mygdxgame;
 
+import java.awt.Event;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -12,16 +13,18 @@ private int xStart;
 private int xEnd;
 private int yStart;
 private int yEnd;
-//Stub
+
 
 public void fly( ){
 
 	if(collide = true){//Collide is from Sayujya's collision class
 		this.setAlive(false);//Base case 
-	}else{// trying to use listener
+	}else{
+		fly();// What? Recursion? Maybe?
 
 	}
 }
+
 public Boolean getAlive() {
 return alive;
 }
@@ -61,9 +64,16 @@ public void setyStart(int yStart) {
 this.yStart = yStart;
 }
 @Override
-public void keyPressed(KeyEvent e) {
-	// TODO Auto-generated method stub
-	
+public void keyPressed(KeyEvent e) {// TRYING TO LEARN TO USE KEY LISTENERS 
+	if(e.getKeyCode() == KeyEvent.VK_LEFT){
+		this.setxStart(this.getxStart()-1);
+		this.setxEnd(this.getxEnd()-1);
+	}
+	else if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+		this.setxStart(this.getxStart()+1);
+		this.setxEnd(this.getxEnd()+1);
+	}
+       
 }
 @Override
 public void keyReleased(KeyEvent e) {
